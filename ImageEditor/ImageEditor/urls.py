885c8f22dcc32984746e8c19bc8551788c3eecc8
Emilia Yavarova <emiliaverova@gmail.com>
2024-11-14 12:28:51 +0400

    has errors, pasha check pls

--- ImageEditor/ImageEditor/urls.py
+++ ImageEditor/ImageEditor/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
+from core.views import HomeView, AboutView, ContactView
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include("core.urls")),
+    path('', RedirectView.as_view(url='core/'))
 ]
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ImageEditor/core/views.py
+++ ImageEditor/core/views.py
@@ -9,40 +9,10 @@ class HomeView(TemplateView):
     '''
     Sets the home page.
     '''
-    template_name = 'index.html'
+    template_name = 'ImageEditor/templates/index.html'
 
 class AboutView(TemplateView):
     '''
     Sets the about page.
     '''
-    template_name = 'about.html'
-
-class ContactView(View):
-    '''
-    Sets the contact page.
-    '''
-    def get(self, request):
-        return render(request, "contact.html")
-    
-    def post(self, request):
-        name = request.POST.get("name")
-        email_id = request.POST.get("email")
-        msg = request.POST.get("message")
-
-        # Manually open the connection
-        connection = mail.get_connection()
-        connection.open()
-        body = f'''Name: {name}\n
-Email: {email_id}\n
-Message: {msg}\n
-        '''
-        # Construct an email message that uses the connection
-        email = mail.EmailMessage(
-            'Imgman Contact Form',
-            body,
-            settings.EMAIL_HOST_USER,
-            ['kaijat108@gmail.com'],
-            connection=connection,
-        )
-        email.send() # Send the email
-        return redirect(reverse_lazy("core:index"))
+    template_name = 'ImageEditor/templates/about.html'
