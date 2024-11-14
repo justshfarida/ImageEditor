2e9dc413c623d3618b162ef9c555d13b810fae90
Pavel418 <pavel@vvv.az>
2024-11-14 12:35:55 +0400

    Update urls.py

--- ImageEditor/ImageEditor/urls.py
+++ ImageEditor/ImageEditor/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
-from core.views import HomeView, AboutView, ContactView
+from core.views import HomeView, AboutView
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
