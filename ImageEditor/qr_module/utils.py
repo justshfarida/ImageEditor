0f5c485d19ea0fcfb86eef4bdb239089b5f1affd
Farida Shakikhanli <turtleowl2005@gmail.com>
2024-11-21 01:57:17 +0400

    Unit Tests of Qr Module

--- ImageEditor/qr_module/utils.py
+++ ImageEditor/qr_module/utils.py
@@ -8,22 +8,22 @@ import requests
 
 # QR Code generator function
 def generate_qr_code(data, fill_color='black', back_color='white'):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(data)
-    qr.make(fit=True)
-    img = qr.make_image(fill_color=fill_color, back_color=back_color)
-    
-    # Save to Cloudinary
-    img.save("temp_qr.png")
-    response = cloudinary.uploader.upload("temp_qr.png", folder="generated_qr_codes")
-    os.remove("temp_qr.png")
-    
-    return response['url']
+    try:
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(data)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color=fill_color, back_color=back_color)
+        
+        # Save to Cloudinary
+        response = cloudinary.uploader.upload(img, folder="generated_qr_codes")
+        return response.get('url', None)
+    except ValueError as e:
+        raise ValueError(f"Error generating QR code: {e}")
 
 # QR Code reader function
 def read_qr_code(image_url):
