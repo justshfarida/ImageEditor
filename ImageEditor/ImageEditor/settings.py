7b2b32e63d7a37d3f754530ea3953f480ccf2f55
Pavel418 <pavel@vvv.az>
2024-12-11 13:33:26 +0400

    Update settings.py

--- ImageEditor/ImageEditor/settings.py
+++ ImageEditor/ImageEditor/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["https://chat.ufaz.az", "chat.ufaz.az", "http://chat.ufaz.az", "https://chat.ufaz.az/"]
+CSRF_TRUSTED_ORIGINS = ["https://chat.ufaz.az", "http://chat.ufaz.az", "https://chat.ufaz.az/"]
 
 # Application definition
 INSTALLED_APPS = [
